1f202f42e538fa7db1fb0e0a8be448308a31f561
Nate West <nayt@Nates-Air.lan>
2023-01-26 12:17:26 -0600

    done

--- app.js
+++ app.js
@@ -18,8 +18,8 @@
         fetch('https://jsonplaceholder.typicode.com/todos')
         .then( (response) => response.json())
         .then( (json) => arrayOfTodos = json)
-        .then( () => populateTodos())
-        .then( () => logTodos())
+        // .then( () => populateTodos())
+        // .then( () => logTodos())
     }
     
     const logTodos = () => {
@@ -41,21 +41,68 @@
         }
     }
 
-    const populateTodos2 = () => {
-        let todoSection = document.getElementById('container');
+    // const populateTodos2 = () => {
+    //     let todoSection = document.getElementById('container');
 
-        let tdTitle = document.createTextNode(arrayOfTodos[0].title);
-        let tdComplete = document.createTextNode(arrayOfTodos[0].completed);
+    //     let tdTitle = document.createTextNode(arrayOfTodos[0].title);
+    //     let tdComplete = document.createTextNode(arrayOfTodos[0].completed);
 
-        let tdLine = `Title: ${tdTitle} status is ${tdComplete}`;
+    //     let tdLine = `Title: ${tdTitle} status is ${tdComplete}`;
 
-        let todoP = document.createElement('P');
+    //     let todoP = document.createElement('P');
 
-        todoP.appendChild(tdLine);
+    //     todoP.appendChild(tdLine);
 
-        todoSection.innerHTML(tdLine);
+    //     todoSection.innerHTML(tdLine);
+    // }
+
+    const getByUser = () => {
+
+        clearTodos();
+
+        const numInput = document.getElementById('num-input').valueAsNumber
+
+        const filteredArray = arrayOfTodos.filter((arr) => arr.userId === numInput && arr.completed == false)
+
+        // console.log(filteredArray)
+
+        for(let i = 0; i < filteredArray.length; i++) {
+    
+            let todoList = document.getElementById('todo-list');
+
+            let todoItem = document.createElement('LI');
+
+            let todoTitle = document.createTextNode(filteredArray[i].userId + " " +filteredArray[i].title);
+
+            todoItem.appendChild(todoTitle);
+            todoList.appendChild(todoItem);
+        }
+    }
+
+    const clearTodos = () => {
+
+        let todos = document.getElementsByTagName("OL")
+        for (i=0; i < todos.length; i++) {
+        console.log(todos[i].id)
+        todos[i].innerHTML = null
+        }
     }
 
     // (response) => {
     //     response.json()
     // }
+
+    // DONE Using the assignment from yesterday, create a branch called: " Todo-Filtering ".
+    // DONE Fetch the same data.
+    // DONE Store the data in a variable.
+    // DONE Add an input for the userID. This input should only take in a number from 1 - 10.
+    // DONE Add a button that when clicked will:
+    // DONE clear the previous todos from the view
+    // and populate it with only todos with the userID that matches the number inputted.
+    // then stores the currently filtered todos in a variable so that ...
+    // You can create two more buttons that when clicked:
+
+    // removes those todos from the view
+    // and shows only todos that are either:
+    // completed
+    // not completed
